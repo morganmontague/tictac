4a34a955cec15ba95f9e02a490858d5184c276e6
C. Morgan Montague <morgan.montague@student.awesomeinc.org>
2022-10-06 01:48:04 +0000

    added a basic function to create the board

--- JS/main.js
+++ JS/main.js
@@ -149,16 +149,26 @@ console.log(winningThree.length)
 console.log(winningThree)
 
 
-// function createElement (area, element, text, clas, parent) {
-//     let id = `${area}id` 
-//     let createElement = document.createElement(element)
-//     parent.appendChild(createElement)
-//     createElement.setAttribute('id',id)
-//     createElement.setAttribute('class', clas)
-//     createElement.innerText = `${text}`
-//   }
+function createElement (area, element, text, clas, parent) {
+    let id = `${area}id` 
+    let createElement = document.createElement(element)
+    parent.appendChild(createElement)
+    createElement.setAttribute('id',id)
+    createElement.setAttribute('class', clas)
+    createElement.innerText = `${text}`
+  }
 
-//   createElement('top', 'div', 'Why is the text broken', 'row', blah)
+  createElement('top', 'div', '', 'row', blah);
+  let rowParent = document.getElementById('topid');
+  console.log(rowParent)
+  for (let index = 0; index < 9; index++) {
+    createElement(`${index}Col`, 'div', `column${index}`, 'col-4 border', rowParent)
+  }
+
+  // for (let index = 0; index < 9; index++){
+  //   let `${index}Column` = document.getElementById(`${index}Colid`)
+  // }
+
 
 
 
