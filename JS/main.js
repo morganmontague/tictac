c33f8711fd2b04a38899a3c634ff22bede820f49
C. Morgan Montague <morgan.montague@student.awesomeinc.org>
2022-10-06 15:30:21 +0000

    managed to fix the scoping for my check for win

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,4 @@
 console.log('running')
-// let x = 1
-// let y = 4
-// let board = [0,0,0,0,0,0,0,0,0]
-// console.log(board.length)
-
 
 
 // let cloneBoard = Array.from(board)
@@ -15,46 +10,9 @@ console.log('running')
 // console.log('changed3',cloneBoard)
 // console.log('og',board)
 
-// function won(a,b,c){
-//     if (cloneBoard[a]+cloneBoard[b]+cloneBoard[c]===3){
-//         console.log('x won')
-//     }
-//     else if (cloneBoard[a]+cloneBoard[b]+cloneBoard[c]===12){
-//         console.log('y won')
-//     }
-//     else {
-//         console.log('lost')
-//     }
-// }
-// won(1,2,3)
 
-// cloneBoard[0]=x
-// cloneBoard[1]=y
-// cloneBoard[2]=x
-// cloneBoard[4]=y
-// cloneBoard[7]=y
-// cloneBoard[8]=x
-// won(0,2,8)
-// won(0,1,2)
-// won(1,4,7)
-// console.log(board)
-// console.log(cloneBoard)
 
-function multiplyAll(arr) {
-    let product = 0;
-    // Only change code below this line
-    for (let i = 0; i < arr.length; i++) {
-      const subArray = arr[i];
-      for (let j = 0; j < subArray.length; j++) {
-        product += subArray[j];
-      }
-    }
-    // Only change code above this line
-    console.log(product)
-    return product;
-  }
-  
-  multiplyAll([[1,2],[3,4],[5,6,7]]);
+
   
   let x = 'x'
   let o = 'o'
@@ -78,12 +36,25 @@ function multiplyAll(arr) {
     }
   }
   )
-  console.log(btn)
-
+function changeTurn() {
+  if (playerTurn % 2 === 0) {
+    tileText.textContent = 'O'
+    console.log('O works')
+    playerTurn = playerTurn +1
+} else if (playerTurn % 2 === 1){
+    console.log('X works')
+    tileText.innerText = 'X'
+    playerTurn = playerTurn+1
+}
+}
 
  
 
   let board = [o,o,x,0,x,0,x,0,0]
+  board = [0,0,0,0,0,0,0,0,0]
+  console.log(board)
+  
+
 
  let winningThree = [
     [board[0], board[1], board[2]],
@@ -95,6 +66,8 @@ function multiplyAll(arr) {
     [board[0], board[4], board[8]],
     [board[2], board[4], board[6]]
 ];
+
+board.splice(5, 1, x)
 
 function areYaWinningSon(arr) {
     for (let i = 0; i < arr.length; i++) {
@@ -119,7 +92,7 @@ function areYaWinningSon(arr) {
 areYaWinningSon(winningThree)
 
 
-function paintBox() {
+function changeBox() {
   /////////////////////////////// figure this OUT this will be the test to see if I can change an attribute of one of the 9 columns and prevent other changes 
 }
 
@@ -164,6 +137,56 @@ attempt.setAttribute('onClick', 'attemptToClick()')
 
 
 
+  let testBoard = 
+  [
+    0,0,0,
+    0,0,0,
+    0,0,0,
+  ]
+  console.log(testBoard)
+  
+
+
+ let winning = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+// for (let i = 0; i < winConditions.length; i++) {
+//   const line = winConditions[i]; // [0,1,2]
+//   console.log('LINE: ', line);
+//   console.log(line[0], line[1], line[2]);
+//   console.log(`BOARDSTATE[${line[0]}]: `, boardState[line[0]])
+//   console.log(`BOARDSTATE[${line[1]}]: `, boardState[line[1]])
+//   console.log(`BOARDSTATE[${line[2]}]: `, boardState[line[2]])
+
+
+function whim() {
+  for (let i = 0; i < winning.length; i++) {
+    let line = winning[i]
+    console.log('line', line)
+      let a = testBoard[line[0]];
+      let b = testBoard[line[1]];
+      let c = testBoard[line[2]];
+      if (a === 0 || b === 0 || c === 0) {
+          console.log('nope')
+          continue;
+      }
+      if (a === 'x' && b === 'x' && c ==='x') {
+          console.log('x won baby')
+          break}
+      if (a === 'o' && b === 'o'&& c==='o') {
+          console.log('o won baby')
+          break}
+  }
+}
+whim()
 
 
 
@@ -177,15 +200,3 @@ attempt.setAttribute('onClick', 'attemptToClick()')
 
 
 
-//   function createRow (area, element, text, clas, parent) {
-//     let id = `${area}id` 
-//     let testing = `${area}row` 
-//     // testing.document.createElement(element)
-//     let parentArea = `${area}Parent`
-//     parentArea.appendChild(testing)
-//     testing.setAttribute('id', id)
-//     testing.setAttribute('class', clas)
-//     testing.innerText = (text)
-//      parentArea = document.getElementById(id)
-//   }
-//   createRow('top', 'div', 'top row', 'row', 'blah')
---
+++ JS/sandbox.js
@@ -0,0 +1,59 @@
+// let x = 1
+// let y = 4
+// let board = [0,0,0,0,0,0,0,0,0]
+// console.log(board.length)
+
+// function won(a,b,c){
+//     if (cloneBoard[a]+cloneBoard[b]+cloneBoard[c]===3){
+//         console.log('x won')
+//     }
+//     else if (cloneBoard[a]+cloneBoard[b]+cloneBoard[c]===12){
+//         console.log('y won')
+//     }
+//     else {
+//         console.log('lost')
+//     }
+// }
+// won(1,2,3)
+
+// cloneBoard[0]=x
+// cloneBoard[1]=y
+// cloneBoard[2]=x
+// cloneBoard[4]=y
+// cloneBoard[7]=y
+// cloneBoard[8]=x
+// won(0,2,8)
+// won(0,1,2)
+// won(1,4,7)
+// console.log(board)
+// console.log(cloneBoard)
+
+
+// function multiplyAll(arr) {
+//     let product = 0;
+//     // Only change code below this line
+//     for (let i = 0; i < arr.length; i++) {
+//       const subArray = arr[i];
+//       for (let j = 0; j < subArray.length; j++) {
+//         product += subArray[j];
+//       }
+//     }
+//     // Only change code above this line
+//     console.log(product)
+//     return product;
+//   }
+  
+//   multiplyAll([[1,2],[3,4],[5,6,7]]);
+
+//   function createRow (area, element, text, clas, parent) {
+//     let id = `${area}id` 
+//     let testing = `${area}row` 
+//     // testing.document.createElement(element)
+//     let parentArea = `${area}Parent`
+//     parentArea.appendChild(testing)
+//     testing.setAttribute('id', id)
+//     testing.setAttribute('class', clas)
+//     testing.innerText = (text)
+//      parentArea = document.getElementById(id)
+//   }
+//   createRow('top', 'div', 'top row', 'row', 'blah')
